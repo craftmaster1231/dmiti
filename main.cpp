60bc515fe4d1cd0c704b790176b3a0a97285a526
Alex <craftmaster1231@gmail.com>
2021-03-30 16:02:56 +0300

    Better comments. Fixed check function

--- main.cpp
+++ main.cpp
@@ -2,22 +2,8 @@
 #include <list>
 #include <vector>
 #include "pf.h"
-template<typename T>
-void printmultitude(const std::list<std::vector<T>>& to_print) {
-    std::cout<<"Pareto multitude:"<<std::endl;
-    auto it = to_print.begin();
-    for(int i=0;i<to_print.size();i++) {
-        std::cout<<"Object:\t";
-        for(int j=0; j<to_print.begin()->size();j++) {
-            std::cout<<(*it)[j]<<" ";
-        }
-        std::cout<<std::endl;
-        ++it;
-    }
-    std::cout<<std::endl;
-}
+#include <forward_list>
 int main() {
-
     std::cout<<"Enter number of parameters for each object"<<std::endl;
     int vectorSize;
     std::cin>>vectorSize;
@@ -37,7 +23,7 @@ int main() {
     }
     std::cout<<std::endl;
     auto Pareto = pf::find_and_alloc_Pareto(base);
-    printmultitude(Pareto);
+    pf::printmultitude(Pareto);
     return 0;
 }
 
--- pf.h
+++ pf.h
@@ -8,20 +8,20 @@
 #include <iostream>
 #include <list>
 #include <vector>
-namespace pf { //Functions for Pareto multitude.
+namespace pf { //Functions to work with Pareto multitude.
     /*
-     * if lhv is better - returns true.
-     * if incomparable  - returns true
-     * if worse         - returns false
-     * if same          - returns true
+     * if lhv is better         - returns true.
+     * if lhv isincomparable    - returns true
+     * if lhv is worse          - returns false
+     * if same                  - returns true
      */
     template<typename T>
     bool check (const std::vector<T>& lhv, const std::vector<T>& rhv) {
         if(lhv.size() != rhv.size()) {
             std::cout<<"check() different sizes!"<<std::endl; //debug thing
         }
-        bool element_more=false; //do we found that any lhv[i] >rhv[i]
-        bool element_less=false;
+        bool element_more=false;    //do we found that any lhv[i] > rhv[i]
+        bool element_less=false;    //do we found that any lhv[i] < rhv[i]
         int size = lhv.size();
         for(int i = 0;i<size;i++){
             if(lhv[i]>rhv[i]) {
@@ -31,14 +31,14 @@ namespace pf { //Functions for Pareto multitude.
             if(lhv[i]<rhv[i]) {
                 element_less = true;
             }
-            if(!element_less && !element_more) {
+            if(element_less && element_more) {
                 return true; //lhv and rhv are incomparable;
             }
         }
         /* if rhv and lhv are incomparable, it will be known in cycle.
          * So, lhv is only better, worse or equal than rhv.
          */
-        if(element_less && element_more){
+        if(!element_less && !element_more){ // are same
             return true;
         }
         if(element_more && !element_less) {
@@ -73,6 +73,21 @@ namespace pf { //Functions for Pareto multitude.
             ++current;
         }
         return std::move(*retn);
+    }
+
+    template<typename T>
+    void printmultitude(const std::list<std::vector<T>>& to_print) {
+        std::cout<<"Pareto multitude:"<<std::endl;
+        auto it = to_print.begin();
+        for(int i=0;i<to_print.size();i++) {
+            std::cout<<"Object:\t";
+            for(int j=0; j<to_print.begin()->size();j++) {
+                std::cout<<(*it)[j]<<" ";
+            }
+            std::cout<<std::endl;
+            ++it;
+        }
+        std::cout<<std::endl;
     }
 }
 #endif //DMITITOCOPY_PF_H
